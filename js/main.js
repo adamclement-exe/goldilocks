670285fae41df82542ece0166272d877876bdf1b
adamclement-exe <adamclement@exe-coll.ac.uk>
2025-04-03 13:52:00 +0100

    UI improvements: Simplified help system, fixed worker points display rounding, removed modal approach for team dynamics

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@ function initializeGame() {
     
     // Show welcome tutorial
     setTimeout(() => {
-        UI.showTutorialModal('achievements');
+        UI.showTutorialModal('help');
     }, 1000);
 
     // --- Start Game with Instructions ---
